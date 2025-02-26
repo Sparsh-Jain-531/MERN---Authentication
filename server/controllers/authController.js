35b43b01d6c8085e4ab87946cfb5b02e1f1039ff
Sparsh-Jain-531 <sparshjain531@gmail.com>
2025-02-26 12:06:03 +0530

    implement email verification and OTP handling; enhance Navbar for verification flow and create EmailVerify component

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,6 +2,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
 import transporter from "../config/nodemailer.js";
+import { EMAIL_VERIFY_TEMPLATE, PASSWORD_RESET_TEMPLATE } from "../config/emailTemplates.js"
 
 export const register = async(req,res)=>{
 
@@ -153,7 +154,8 @@ export const sendVerifyOtp = async(req,res)=>{
             from: process.env.EMAIL_USER,
             to: user.email,
             subject: "Account Verification OTP",
-            text: `Your otp is ${otp}. Verify your account using this OTP.`,
+            // text: `Your otp is ${otp}. Verify your account using this OTP.`,
+            html: EMAIL_VERIFY_TEMPLATE.replace("{{otp}}",otp).replace("{{email}}",user.email)
         };
 
         await transporter.sendMail(mailOptions);
@@ -267,7 +269,8 @@ export const sendResetOtp = async(req, res) => {
             from: process.env.EMAIL_USER,
             to: user.email,
             subject: "Password Reset OTP",
-            text: `Your OTP for resetting your password is ${otp}. Use this otp to reset your Password`,
+            // text: `Your OTP for resetting your password is ${otp}. Use this otp to reset your Password`,
+            html: PASSWORD_RESET_TEMPLATE.replace("{{otp}}",otp).replace("{{email}}",user.email)
         };
 
         await transporter.sendMail(mailOptions);
